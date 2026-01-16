7778a648a24eb3cbbb6f56292a93ca32f8610853
Alex Lopez España <a20000lex@gmail.com>
2026-01-16 02:47:43 +0100

    Subir cambios

--- batoibooks/src/components/BookForm.vue
+++ batoibooks/src/components/BookForm.vue
@@ -1,69 +1,94 @@
 <script setup>
+import { onMounted, reactive, ref } from 'vue';
+import { store as modulos } from '../stores/modulos.js';
+import { store as libros } from '../stores/libros.js';
+
+onMounted(async () => {
+    await modulos.allModulos();
+    await libros.allLibros();
+});
+
+const nuevoLibro = reactive({
+    id: null,
+    idModule: '',
+    publisher: '',
+    price: 0,
+    pages: 0,
+    status: 'Nuevo',
+    comments: '',
+    photo: '',
+    soldDate: ''
+});
+
+
+
+const resetearFormulario = () => {
+        nuevoLibro.id= null,
+        nuevoLibro.idModule= '',
+        nuevoLibro.publisher= '',
+        nuevoLibro.price= 0,
+        nuevoLibro.pages= 0,
+        nuevoLibro.status= 'Nuevo',
+        nuevoLibro.comments= '',
+        nuevoLibro.photo= '',
+        nuevoLibro.soldDate= ''
+}
+
+const crearLibro = async () => {
+    await libros.addLibro(nuevoLibro);
+    resetearFormulario();
+}
 
 </script>
 
 <template>
     <div class="formularioLibro" id="formularioLibro">
         <h2>Agregar Nuevo Libro</h2>
-        <form action="">
+        <form @submit.prevent="crearLibro">
             <div>
                 <h4>ID:</h4>
-                <input>
+                <input placeholder="ID del libro">
             </div>
             <div>
                 <h4>Module:</h4>
-                <select name="modulo" id="modulo" required>
-                    <option value="">Seleccione un módulo</option>
-                    <option value="Programación">Programación</option>
-                    <option value="Base de Datos">Base de Datos</option>
-                    <option value="Redes">Redes</option>
-                    <option value="Sistemas">Sistemas</option>
+                <select v-model="nuevoLibro.idModule" required>
+                    <option value="">Selecciona un módulo</option>
+                    <option v-for="modulo in modulos.state.modulos" :key="modulo.id" :value="modulo.code">
+                        {{ modulo.cliteral }}
+                    </option>
                 </select>
+
             </div>
             <div>
                 <h4>Editorial:</h4>
-                <input type="text" name="editorial" id="editorial" placeholder="Ingrese la editorial (libro)" required>
+                <input type="text" name="editorial" id="editorial" placeholder="Ingrese la editorial (libro)" required
+                    v-model="nuevoLibro.publisher">
             </div>
             <div>
                 <h4>Precio:</h4>
                 <input type="number" name="precio" id="precio" min="0" step="0.01" placeholder="Precio del libro"
-                    required>
+                    v-model="nuevoLibro.price" required>
             </div>
             <div>
                 <h4>Páginas</h4>
-                <input type="number" name="paginas" id="paginas" min="1" placeholder="Número de páginas" required>
+                <input type="number" name="paginas" id="paginas" min="1" placeholder="Número de páginas"
+                    v-model="nuevoLibro.pages" required>
 
             </div>
             <div>
                 <h4>Estado</h4>
-                <label>
-                    <input type="radio" name="estado" value="Nuevo">
-                    Nuevo
-                </label>
-
-                <label>
-                    <input type="radio" name="estado" value="Bueno">
-                    Bueno
-                </label>
-
-                <label>
-                    <input type="radio" name="estado" value="Malo">
-                    Malo
-                </label>
-
-                <label>
-                    <input type="radio" name="estado" value="Digital">
-                    Digital
-                </label>
+                <label><input type="radio" value="Nuevo" v-model="nuevoLibro.status">Nuevo</label>
+                <label><input type="radio" value="Bueno" v-model="nuevoLibro.status">Bueno</label>
+                <label><input type="radio" value="Malo" v-model="nuevoLibro.status">Malo</label>
+                <label><input type="radio" value="Digital" v-model="nuevoLibro.status">Digital</label>
             </div>
             <div>
                 <h4 for="comentario">Comentarios</h4>
-                <textarea id="comentario" name="comentario" rows="4" cols="50">Escribe tu comentario aquí...
-                </textarea>
+                <textarea v-model="nuevoLibro.comments" rows="4" cols="50" placeholder="Escribe tu comentario aquí..."></textarea>
             </div>
             <div>
-                <button>Guardar</button>
-                <button>Reset</button>
+                <button type="submit">Guardar</button>
+                <button type="reset" @click="resetearFormulario">Reset</button>
 
             </div>
 
--- batoibooks/src/components/BooksList.vue
+++ batoibooks/src/components/BooksList.vue
@@ -7,10 +7,18 @@ import { store as libros } from '../stores/libros.js';
 
 
 const totalLibros = computed(() => libros.state.libros.length);
+
+const totalImporte = computed(() => {
+  return libros.state.libros.reduce((acc, libro) => {
+    return acc + (libro.price || 0); // si no hay price, suma 0
+  }, 0);
+});
+
+
 onMounted(async () => {
     await libros.allLibros();
 });
-const borrarLibro =async (idLibro)=>{
+const borrarLibro = async (idLibro) => {
     await libros.removeLibro(idLibro);
 }
 
@@ -22,7 +30,7 @@ const borrarLibro =async (idLibro)=>{
         <BookItem v-for="libro in libros.state.libros" :key="libro.id" :libro="libro" @borrarLibro="borrarLibro" />
     </div>
     <p class="totalLibros">Total de libros:{{ totalLibros }}</p>
-    <p class="totalImporte">Importe Total:€</p>
+    <p class="totalImporte">Importe Total:{{totalImporte}}€</p>
 </template>
 
 
