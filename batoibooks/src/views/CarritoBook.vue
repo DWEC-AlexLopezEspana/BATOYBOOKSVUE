f149d50bf6fe3bd279be73e9a4c08d176b64dd34
Sitoosito8 <a20000lex@gmail.com>
2026-01-29 16:32:15 +0100

    subir cambio nueva rama que es pinia

--- batoibooks/src/views/CarritoBook.vue
+++ batoibooks/src/views/CarritoBook.vue
@@ -5,7 +5,6 @@
 <template>
     <div>
         <h2>Carrito Compra 🛒</h2>
-        <p>Este es actualmente el carrito de compra que tienes </p>
     </div>
     <div>
         <p>Alex López España</p>
